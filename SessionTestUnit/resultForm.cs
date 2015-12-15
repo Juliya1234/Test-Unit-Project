c27537e652a97442b70cb5cdf1339afb5299c080
Maxim <maximgorbatyuk191093@gmail.com>
2015-12-15 09:19:14 +0600

    Some fixes

--- SessionTestUnit/resultForm.cs
+++ SessionTestUnit/resultForm.cs
@@ -25,7 +25,19 @@ namespace SessionTestUnit
         {
             double result = (double)rigth / count;
             result = result * 100;
-            label2.Text = result.ToString("#.##") + "%";
+            label2.Text = result.ToString("#.##") + "%\r\n" + get_swearing(result);
+        }
+        private string get_swearing(double res)
+        {
+            if (res >= 90)
+                return "Круто, долбанный ботаник";
+            if ((res >= 75) && (res < 90))
+                return "Слабовато.\r\nДаже твоя жирная мамаша может лучше";
+            if ((res >= 60) && (res < 75))
+                return "Да ты шутишь?\r\nАутист с 4 классами образования смог бы лучше";
+            if ((res >= 50) && (res < 60))
+                return "Хуже уже быть не может, дубина";
+            return "Просто нет слов";
         }
     }
 }
